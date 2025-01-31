c0620a31d38259b342c3a5c34d2cf6e7779f970d
Tamin Mahboubi <taminmahboubi@gmail.com>
2025-01-31 21:03:17 +0100

    Modified the script to handle missing directory

--- log_cleaner.sh
+++ log_cleaner.sh
@@ -4,6 +4,13 @@
 LOG_DIR="/var/log/myapp"
 RETENTION_DAYS=7
 
+# Make sure the directory exists
+if [ ! -d "$LOG_DIR" ]; then
+    echo "Error: Log directory $LOG_DIR does not exist!"
+    exit 1
+fi
+
+
 # Find and compress old log files
 find "$LOG_DIR" -type f -mtime +$RETENTION_DAYS -name "*.log" -exec gzip {} \;
 
