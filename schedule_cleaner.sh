12d7a8f3f5228d65b0747536e596a35fe7b6aafa
Tamin Mahboubi <taminmahboubi@DESKTOP-FMC8PN8.localdomain>
2026-02-18 11:20:46 +0000

    refactor: add duplicate check to prevent multiple cron entries

--- schedule_cleaner.sh
+++ schedule_cleaner.sh
@@ -9,7 +9,15 @@ chmod +x "$SCRIPT_PATH"
 # Define the cron job (minute, hour, day of month, month, day of week)
 CRON_JOB="0 0 * * 0 $SCRIPT_PATH"
 
-# append the crontab and feed it back into the system, 
-# '2>/dev/null' to avoid the 'no crontab for user' error for first time users
-(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab - 
-echo "SUCCESS: Log cleaner scheduled for every Sunday at midnight!"
+
+# check if the job already exists to avoid duplicates
+if crontab -l 2>/dev/null | grep -Fq "$SCRIPT_PATH"; then
+       echo "SKIP: log cleaner is already scheduled in crontab."
+else
+
+	# append the crontab and feed it back into the system, 
+	# '2>/dev/null' to avoid the 'no crontab for user' error for first time users
+	(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab - 
+	echo "SUCCESS: Log cleaner scheduled for every Sunday at midnight!"
+fi
+
