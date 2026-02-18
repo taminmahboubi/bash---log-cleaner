f3e562f294f9641c44dbffd5ac88bcc69f60e9c3
Tamin Mahboubi <taminmahboubi@DESKTOP-FMC8PN8.localdomain>
2026-02-18 11:35:15 +0000

    refactor: add addtional check for executable permissions

--- schedule_cleaner.sh
+++ schedule_cleaner.sh
@@ -3,8 +3,11 @@
 # get the absolute path using 'pwd' command using 'command substitution'
 SCRIPT_PATH="$(pwd)/log_cleaner.sh"
 
-# make sure the log cleaner is executable
-chmod +x "$SCRIPT_PATH"
+
+if [ ! -x "$SCRIPT_PATH" ]; then
+	echo "Fixing permissions: Making log_cleaner.sh executable..."
+	chmod +x "$SCRIPT_PATH"
+fi
 
 # Define the cron job (minute, hour, day of month, month, day of week)
 CRON_JOB="0 0 * * 0 $SCRIPT_PATH"
@@ -17,6 +20,7 @@ else
 
 	# append the crontab and feed it back into the system, 
 	# '2>/dev/null' to avoid the 'no crontab for user' error for first time users
+	
 	(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab - 
 	echo "SUCCESS: Log cleaner scheduled for every Sunday at midnight!"
 fi
